16a3b60d685614688559cf60a67f0636c2f43c0e
tasnimsassi <sassitasnim631@gmail.com>
2024-04-29 18:22:51 +0100

    final commit

--- font-angular/src/app/config/my-app-config.ts
+++ font-angular/src/app/config/my-app-config.ts
@@ -1,7 +1,7 @@
 export default {
 
     oidc: {
-        clientId: '0oag5kxk2cfJ7lmRY5d7',
+        clientId: '0oag5kxk2cfJ7lmRY5d7', //authentification pour okta
         issuer: 'https://dev-16982960.okta.com/oauth2/default',
         redirectUri: 'http://localhost:4200/login/callback',
         scopes: ['openid', 'profile', 'email'],
